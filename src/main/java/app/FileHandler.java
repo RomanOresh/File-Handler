86feca5216072c2969778fce9add1a0500d42f5c
Roman <romanoreshchenko@gmail.com>
2024-10-26 12:05:43 +0200

    Initial commit

---
+++ src/main/java/app/FileHandler.java
@@ -0,0 +1,32 @@
+package app;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandler {
+
+    private final static String BASE_PATH = "files/";
+
+    public String writeFile(String filename ,String fileContent) {
+        try(FileWriter fw = new FileWriter( BASE_PATH + filename + ".txt")) {
+            fw.write(fileContent);
+            return "Success.";
+        } catch (Exception e) {
+            return "Error";
+        }
+    }
+
+    public String readFile(String path) {
+        try (FileReader reader = new FileReader(path)) {
+            int sym;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((sym = reader.read()) != -1) {
+                stringBuilder.append((char) sym);
+            }
+            return stringBuilder.toString();
+        } catch (IOException ex) {
+            return ex.getMessage();
+        }
+    }
+}
